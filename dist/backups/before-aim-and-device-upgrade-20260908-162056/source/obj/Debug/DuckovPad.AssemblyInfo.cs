1de2d949038f330542c8494772641a3154f2260c
aaron <a_j_horstman@hotmail.com>
2026-09-08 17:57:44 -0400

    update assembly informational version and cache files

--- dist/backups/before-aim-and-device-upgrade-20260908-162056/source/obj/Debug/DuckovPad.AssemblyInfo.cs
+++ dist/backups/before-aim-and-device-upgrade-20260908-162056/source/obj/Debug/DuckovPad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DuckovPad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.2.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.2.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.2.0+2e9eae064e24f56636306199d4b89e8076764ed3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DuckovPad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DuckovPad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.2.0.0")]
--- tests/ControllerChecks/obj/Debug/net10.0/ControllerChecks.AssemblyInfo.cs
+++ tests/ControllerChecks/obj/Debug/net10.0/ControllerChecks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControllerChecks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e9eae064e24f56636306199d4b89e8076764ed3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControllerChecks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControllerChecks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
